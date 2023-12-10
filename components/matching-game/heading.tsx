e8b8ba747b282be2bb21e2cab3826a3f971e522c
EvrenTank <evrentank@gmail.com>
2023-12-10 17:50:54 +0300

    matching game mostly completed

--- components/matching-game/heading.tsx
+++ components/matching-game/heading.tsx
@@ -2,31 +2,46 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import { useEffect } from "react";
 
-
-const Heading = ({setColRowNumbers,colRowNumbers}:any) => {
+const Heading = ({setColRowNumbers,colRowNumbers,updateComponents,setDisabled,playerturn,score,setScore,setPlayerturn}:any) => {
 
     const handleSelect = (eventKey:string | null,event:any) => {
        const choosenValues = eventKey!.split("x");
        const rowNumber = choosenValues[0];
        const colNumber = choosenValues[1];
+       console.log("rowNumber: " + rowNumber);
+       console.log("colNumber: " + colNumber);
        setColRowNumbers({...colRowNumbers,rowNumber:rowNumber,colNumber:colNumber});
-    
-    }
+       console.log("colRowNumbers.rowNumber: " + colRowNumbers.rowNumber);
+       console.log("colRowNumbers.colNumber: " + colRowNumbers.colNumber);
+       setDisabled([]); 
+       setScore( (prevScore:any) =>{ 
+        return {...prevScore,first:0,second:0}});
+       setPlayerturn(true);
+;   }
 
+
+    useEffect(()=>{
+        updateComponents(colRowNumbers);
+    },[colRowNumbers]);
+    
     return (
         <Card className="text-center d-flex justify-content-center align-items-center position-relative ">
             <Card.Header as="h4" className="text-danger">Matching Game</Card.Header>
-            <Card.Title>"Matching Game"</Card.Title>
-            <Card.Text>Sıradaki Oyuncu:{true ? "X":"O"}</Card.Text>
-            <Button className="d-inline" variant="outline-info" size="lg" onClick={()=>{}}>RESET</Button>
-            <DropdownButton className="position-absolute top-50 end-0 translate-middle-y z-3" title="Choose sizes" onSelect={handleSelect}>
+            <Card.Title className="d-flex justify-content-around gap-5">
+                <Card.Text>Player 1:{score.first}  Player 2:{score.second}</Card.Text>             
+                <DropdownButton className="z-3align-self-end" title="Choose sizes" onSelect={handleSelect}>
                     <Dropdown.Item eventKey="3x2">3x2</Dropdown.Item>
                     <Dropdown.Item eventKey="4x2">4x2</Dropdown.Item>
                     <Dropdown.Item eventKey="4x3">4x3</Dropdown.Item>
                     <Dropdown.Item eventKey="5x3">5x3</Dropdown.Item>
                     <Dropdown.Item eventKey="5x4">5x4</Dropdown.Item>
-            </DropdownButton>  
+            </DropdownButton> 
+            </Card.Title>
+            <Card.Text className="w-100">Sıradaki Oyuncu:{playerturn ? "Player1":"Player2"}
+            </Card.Text>
+ 
 
         </Card> 
     );
--- components/matching-game/matchingGame.tsx
+++ components/matching-game/matchingGame.tsx
@@ -19,7 +19,11 @@ const MatchingGame = () => {
         }
     });
     const [disabled,setDisabled] = useState<any>([]);
-    
+    const [playerturn,setPlayerturn] = useState<any>(true);
+    const [score,setScore] = useState<{firstPlayerScore:number,secondPlayerScore:number}>({
+        first:0,
+        second:0
+    })  
 
     const check = (imgUrl:string,index:string) => {
         if(choosentwo.first.imgUrl == null && choosentwo.second.imgUrl == null){
@@ -35,26 +39,35 @@ const MatchingGame = () => {
                 console.log("disabled",disabled);
                 setChoosentwo({...choosentwo,first:{imgUrl:imgUrl,index:index},second:{imgUrl:null,index:null}});
                 console.log("choosentwo",choosentwo.first.imgUrl);
+                 // bu yapiya dikkat, return eklemeyince sikinti yasaniyor.
+                if(playerturn){
+                    setScore( (prevScore:any) =>{ 
+                        return {...prevScore,first:prevScore.first+1}});
+                }
+                else if(!playerturn) {
+                    setScore( (prevScore:any) =>{ 
+                        return {...prevScore,second:prevScore.second+1}});                }
             }
             else if(choosentwo.first.imgUrl != choosentwo.second.imgUrl && choosentwo.first.index != index && choosentwo.second.index != index) {
-                setChoosentwo({...choosentwo,first:{imgUrl:imgUrl,index:index},second:{imgUrl:null,index:null}});            }
+                setChoosentwo({...choosentwo,first:{imgUrl:imgUrl,index:index},second:{imgUrl:null,index:null}}); 
+                setPlayerturn((playerturn:boolean)=>!playerturn)           }
         }
         console.log("choosentwo==",choosentwo);
     }
 
     const [colRowNumbers,setColRowNumbers] = useState<{rowNumber:number,colNumber:number}>( {
-        rowNumber:2,
-        colNumber:3
+        rowNumber:3,
+        colNumber:2
     });
     const [components,setComponents] = useState<{rowComponents:string[],colComponents:string[]}>(
         {rowComponents:[],
          colComponents:[]});
     const [imageurls,setImageurls] = useState<string[]>([]);
-    const updateComponents = (newcolRowNumbers:any) => {
 
+    // row ve col number degistiginde componentlari ve imagelari tekrar olusturuyor.
+    const updateComponents = (newcolRowNumbers:any) => {
         const rowArray:string[] = [];
         const colArray:string[] = [];
-
         for(let i=0;i<newcolRowNumbers.rowNumber;i++){
             rowArray.push(`${i}`);
         }
@@ -62,7 +75,6 @@ const MatchingGame = () => {
             colArray.push(`${i}`);
         }
         setComponents(components => ({...components, rowComponents:rowArray, colComponents:colArray}));
-        
         const neededImagesNumber = newcolRowNumbers.rowNumber * newcolRowNumbers.colNumber /2;
         const slicedArray = [...imgDirectoriesArray.slice(0,neededImagesNumber),...imgDirectoriesArray.slice(0,neededImagesNumber)];
         const shuffledArray = shuffleArray(slicedArray);
@@ -72,16 +84,14 @@ const MatchingGame = () => {
     useEffect(()=>{
         updateComponents(colRowNumbers);
         setChoosentwo({...choosentwo,first:{imgUrl:null,index:null},second:{imgUrl:null,index:null}});
-        setDisabled([]);
     },[colRowNumbers.colNumber,colRowNumbers.rowNumber,colRowNumbers]);
-    useEffect(()=>{ // Buralardan kaynaklanan hatalar var. Nasıl duzeltilecek bilmiyorum.
-        console.log("disabled",disabled);
-    },[disabled])
-
     return (
 
                 <Container className='d-grid gap-2 col-12 col-md-6 col-lg-4'>
-                <Row> <Heading setColRowNumbers={setColRowNumbers} colRowNumbers={colRowNumbers} /> </Row>
+                <Row> <Heading setColRowNumbers={setColRowNumbers} colRowNumbers={colRowNumbers} updateComponents={updateComponents} setDisabled={setDisabled}
+                playerturn={playerturn}
+                score={score} setScore={setScore} setPlayerturn={setPlayerturn}
+                /> </Row>
                 {components.rowComponents && components.rowComponents.map((rowComponent,index1)=>{
                     return ( 
                         <Row key={`${index1} ${colRowNumbers.colNumber} ${colRowNumbers.rowNumber}`}>
