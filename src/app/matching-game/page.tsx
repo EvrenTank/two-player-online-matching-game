283d9d1e60466b522d4ba8d5c79bb34481f9788e
EvrenTank <evrentank@gmail.com>
2024-01-27 21:38:08 +0300

    socket connection degistirildi

--- src/app/matching-game/page.tsx
+++ src/app/matching-game/page.tsx
@@ -4,7 +4,7 @@ import MatchingGame from '../../../components/matching-game/matchingGame';
 import Navbar from '../../../components/homePage/navbar';
 import {io,Socket} from "socket.io-client";
 import { useEffect,useState } from 'react';
-const socket:Socket = io.connect("http://localhost:3001");
+const socket:Socket = io.connect("https://server-iosocket.vercel.app");
 
 const Page=()=> {
 
